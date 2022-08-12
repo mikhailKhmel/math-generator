ed59f8715099b2c07c498a0281f4d5900b0fb93e
Mikhail <mikhail.khmel@gmail.com>
2022-08-12 17:59:19 +0300

    Вместо выбора количества примеров используются захардкоженные кнопки

--- src/components/main-mode/main-mode.jsx
+++ src/components/main-mode/main-mode.jsx
@@ -1,11 +1,11 @@
 import {
   Button,
+  ButtonGroup,
   Container,
   IconButton,
   Menu,
   MenuItem,
   Stack,
-  TextField,
   Typography,
 } from '@mui/material';
 import {useState} from 'react';
@@ -19,11 +19,10 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ProfileDialog from '../dialogs/profile-dialog.jsx';
 
 function MainMode(props) {
-  const [examplesCount, setExamplesCount] = useState(10);
+  const [examplesCount, setExamplesCount] = useState(20);
   const [open, setOpen] = useState(false);
   const [openProfile, setOpenProfile] = useState(false);
   const [startExamplesTour, setStartExamplesTour] = useState(false);
-  const [error, setError] = useState('');
   const [openStats, setOpenStats] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const openMenu = Boolean(anchorEl);
@@ -33,15 +32,6 @@ function MainMode(props) {
   const handleCloseMenu = () => {
     setAnchorEl(null);
   };
-
-  function checkExamplesCount() {
-    if (examplesCount > 50 || examplesCount < 1) {
-      setError('Количество примеров не может быть больше 50 и меньше 1');
-      return false;
-    }
-    setError('');
-    return true;
-  }
 
   function handleAnswer(answer) {
     setOpen(false);
@@ -68,7 +58,7 @@ function MainMode(props) {
         <ProfileDialog open={openProfile} action={() => setOpenProfile(false)}/>
         <Stack spacing={3}>
           <Stack direction="row" justifyContent="space-between">
-            <Typography variant="h6">Выбери уровень сложности</Typography>
+            <Typography variant="h6"></Typography>
             <IconButton onClick={handleOpenMenu}><MenuIcon/></IconButton>
             <Menu anchorEl={anchorEl} open={openMenu} onClose={handleCloseMenu}>
               <MenuItem onClick={() => {
@@ -82,17 +72,22 @@ function MainMode(props) {
             </Menu>
           </Stack>
 
-          <TextField
-              error={error !== ''}
-              helperText={error}
-              onChange={(event) => setExamplesCount(
-                  parseInt(event.target.value))}
-              InputLabelProps={{
-                shrink: true,
-              }} value={examplesCount || ''} type="number" variant="standard"
-              label="Сколько примеров?"/>
-          <Button onClick={() => checkExamplesCount() ? setOpen(true) : null}
-                  color="success"
+          <Stack alignItems="center">
+            <Typography variant="h6">Сколько примеров будем решать?</Typography>
+            <ButtonGroup>
+              <Button variant={examplesCount === 10 ? 'contained' : 'outlined'}
+                      color="error"
+                      onClick={() => setExamplesCount(10)}>10</Button>
+              <Button variant={examplesCount === 15 ? 'contained' : 'outlined'}
+                      onClick={() => setExamplesCount(15)}
+                      color="warning">15</Button>
+              <Button variant={examplesCount === 20 ? 'contained' : 'outlined'}
+                      onClick={() => setExamplesCount(20)}
+                      color="success">20</Button>
+            </ButtonGroup>
+          </Stack>
+          <Button onClick={() => setOpen(true)}
+                  color="primary"
                   variant="contained">ПОЕХАЛИ!</Button>
 
         </Stack>
